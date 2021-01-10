93f4157ee077614977e789b97dbfae0210330dfe
Kevin Skoglund <kevin@novafabrica.com>
2021-01-10 16:58:03 -0500

    Add solution to profile photo challenge

--- php_sandbox/user/upload_photo.php
+++ php_sandbox/user/upload_photo.php
@@ -23,12 +23,37 @@ $image_dir = __DIR__ . '/images';
 if(isset($_POST['submit'])) {
 	// process the form data
 
+	$filename = basename($_FILES['file_upload']['name']);
+	$tmp_path = $_FILES['file_upload']['tmp_name'];
+	$type = $_FILES['file_upload']['type'];
+	$size = $_FILES['file_upload']['size'];
+	$error = $_FILES['file_upload']['error'];
+
 	// check for upload errors
+	if($error > 0) {
+		$message = upload_error($error);
+
 	// validate file type
+	} elseif(!in_array($type, $image_types)) {
+		$message = 'File must be an image file.';
+
 	// validate file size
-	// choose a new file name if desired
-	// move file to permanent location
-	// change file permissions
+	} elseif($size > 1000000) {
+		$message = 'File must be less than 1MB.';
+
+	} else {
+		// choose a new file name if desired
+		$target_path = "{$image_dir}/{$filename}";
+
+		// move file to permanent location
+		if(move_uploaded_file($tmp_path, $target_path)) {
+			// change file permissions
+			chmod($target_path, 0766);
+			$message = "Profile photo uploaded.";
+		} else {
+			$message = "Profile photo upload failed.";
+		}
+	}
 
 }
 
@@ -47,8 +72,9 @@ if(isset($_POST['submit'])) {
 		<?php if(!empty($message)) { echo "<p>{$message}</p>"; } ?>
 
 		<!-- Make this form work for uploading a file -->
-		<form action="upload_photo.php">
-
+		<form action="upload_photo.php" method="POST" enctype="multipart/form-data">
+			<input type="hidden" name="MAX_FILE_SIZE" value="1000000" /><br />
+			<input type="file" name="file_upload" /><br />
 			<input type="submit" name="submit" value="Upload" />
 		</form>
 
