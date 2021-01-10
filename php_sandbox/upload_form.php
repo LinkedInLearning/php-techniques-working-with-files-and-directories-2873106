93256cd6b3d292227065b43c7036469e563fe1c4
Kevin Skoglund <kevin@novafabrica.com>
2021-01-10 15:45:21 -0500

    Finish movie 03_02

---
+++ php_sandbox/upload_form.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>File Upload</title>
+  </head>
+  <body>
+
+    <form action="upload.php" method="POST" enctype="multipart/form-data">
+      <input type="file" name="file_upload" /><br />
+      <input type="submit" name="submit" value="Upload" />
+    </form>
+
+  </body>
+</html>
