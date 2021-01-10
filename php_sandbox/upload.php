e561246718bebcc993c2fc401c15288df7e0b592
Kevin Skoglund <kevin@novafabrica.com>
2021-01-10 16:13:25 -0500

    Finish movie 03_04

--- php_sandbox/upload.php
+++ php_sandbox/upload.php
@@ -1,15 +1,20 @@
 <?php
 
+$upload_dir = __DIR__ . '/uploads';
+
 if(isset($_POST['submit'])) {
 
   // process the form data
 
-  echo "<pre>";
-  print_r($_FILES['file_upload']);
-  echo "</pre>";
+  $tmp_path = $_FILES['file_upload']['tmp_name'];
+  $filename = basename($_FILES['file_upload']['name']);
+  $target_path = $upload_dir . '/' . $filename;
 
-  $data = file_get_contents($_FILES['file_upload']['tmp_name']);
-  echo nl2br($data);
+  if(move_uploaded_file($tmp_path, $target_path)) {
+    echo "File upload succeeded.";
+  } else {
+    echo "File upload failed.";
+  }
 }
 
 ?>
